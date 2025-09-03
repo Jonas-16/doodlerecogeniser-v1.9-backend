bfc001a755b947ded8ec01679b38fd8a7abb1080
Jonas-16 <Jonas@vaz.in>
2025-09-03 06:43:12 +0530

    username schema update

--- schemas.py
+++ schemas.py
@@ -79,12 +79,14 @@ class UserCreate(BaseModel):
     username: str
     password: str
 
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
 class UserResponse(BaseModel):
     user_id: int
     username: str
-
-    class Config:
-        orm_mode = True
+    model_config = {"from_attributes": True}
 
 class ImageInput(BaseModel):
     image: List[float]
