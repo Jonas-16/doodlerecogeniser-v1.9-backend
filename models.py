042adbb88707f37109b6361d0a32cb8d07f42402
Jonas-16 <Jonas@vaz.in>
2025-09-03 10:14:00 +0530

    updated models

--- models.py
+++ models.py
@@ -92,7 +92,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
     password_hash = Column(String, nullable=False)
-
     predictions = relationship("PredictionHistory", back_populates="user")
 
 
